ddfc97292a8f12a83075ebad73b4aa55376d22bc
Markeljan Sokoli <Markeljan19@gmail.com>
2022-06-25 20:19:18 -0400

    MintingDone

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-////////////////////////////
-///////Run Code Here////////
-///////////////////////////
-async function main() {
-  //contract readOnlyContract = new ethers(/*Contract Address*/, /*Contract ABI*/, provider);
-  //contract writeContract = new ethers(/*Contract Address*/, /*Contract ABI*/, signer);
-  //see ethers.js docs for how to call contract functions.
-  
-
-}
-
-
+let provider;
+let signer;
+let account;
 
 function init() {
     try {
@@ -27,41 +18,49 @@ document.getElementById("btn-disconnect").addEventListener("click", onDisconnect
 };
 
 async function fetchAccountData() {
-    try {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        let account = await provider.send("eth_requestAccounts").then( accounts => {
-          return accounts[0];});
-        let balance = await provider.getBalance(account);
-        let formatedBalance = ethers.BigNumber.from(balance);
-        formatedBalance = balance.mod(1e14);
-        formatedBalance = ethers.utils.formatEther(balance.sub(formatedBalance));
-        
-        //updateHTMLElements network/balances/button
-        document.getElementById("selected-account").innerHTML = `(${account})`;
-        document.getElementById("account-balance").innerHTML = `${formatedBalance} ${chainMap[ethereum.networkVersion].symbol}`;
-        document.getElementById("network-name").innerHTML = `${chainMap[ethereum.networkVersion].name}`;
-        document.getElementById("btn-connect").style.display = "none";
-        document.getElementById("btn-disconnect").style.display = "block";
-        localStorage.setItem("CACHED_PROVIDER", "TRUE");
-    } catch (error) {
-        console.log("Error connecting to metamask account:\n", error)
-      }
+  try {
+      provider = new ethers.providers.Web3Provider(ethereum);
+      signer = provider.getSigner();
+      account = await provider.send("eth_requestAccounts").then( accounts => {
+        return accounts[0];});
+      let balance = await provider.getBalance(account);
+      let formatedBalance = ethers.BigNumber.from(balance);
+      formatedBalance = balance.mod(1e14);
+      formatedBalance = ethers.utils.formatEther(balance.sub(formatedBalance));
 
-  ethereum.on("accountsChanged", (accounts) => {
-      if(accounts[0]) {
-        fetchAccountData();
-      } else {
-        localStorage.removeItem("CACHED_PROVIDER");
-        document.getElementById("btn-disconnect").style.display = "none";
-        document.getElementById("btn-connect").style.display = "block";
-      }
-  });
-  ethereum.on("chainChanged", (chainId) => {
-    fetchAccountData();
-  });
+      document.getElementById("selected-account").innerHTML = `(${account})`;
+      document.getElementById("account-balance").innerHTML = `${formatedBalance} ${chainMap[ethereum.networkVersion].symbol}`;
+      document.getElementById("network-name").innerHTML = `${chainMap[ethereum.networkVersion].name}`;
+      document.getElementById("btn-connect").style.display = "none";
+      document.getElementById("btn-disconnect").style.display = "block";
+      localStorage.setItem("CACHED_PROVIDER", "TRUE");
+  } catch (error) {
+      console.log("Error connecting to metamask account:\n", error)
+      return
+    }
 
-  main();
+ethereum.on("accountsChanged", (accounts) => {
+    if(accounts[0]) {
+      fetchAccountData();
+    } else {
+      localStorage.removeItem("CACHED_PROVIDER");
+      document.getElementById("btn-disconnect").style.display = "none";
+      document.getElementById("btn-connect").style.display = "block";
+    }
+});
+ethereum.on("chainChanged", (chainId) => {
+  fetchAccountData();
+});
+
+////////////////////
+///MAIN FUNCTIONS///
+////////////////////
+
+let WARRIOR_MINTER = new ethers.Contract(addresses.WARRIOR_MINTER, abis.WARRIOR_MINTER, signer);
+
+
+//await WARRIOR_MINTER.safeMint(account, 2);
+
 };
 
 function onDisconnect() {
